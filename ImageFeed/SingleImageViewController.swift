dea8a3a9e4d7313c985c6bec11951d6a6c3d2187
fortavey <fortavey@gmail.com>
2025-04-14 10:06:26 +0300

    Finish sprint_09

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,86 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Main on 02.03.2025.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var shareButtonView: UIButton!
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded, let image else { return }
+
+            imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.shareButtonView.setTitle("", for: .normal)
+        
+        if let image {
+            self.imageView.image = image
+            self.imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+       
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
